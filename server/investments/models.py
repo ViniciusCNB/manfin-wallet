3c5eb18fffde101dca8a8f2b937e061ea7944949
Vinícius <viniborg25@gmail.com>
2023-06-22 20:44:51 -0300

    criando os models Instituicao e Acao

--- server/investments/models.py
+++ server/investments/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+from django.db.models import Model, CharField, OneToOneField, CASCADE, DateField, IntegerField, FloatField
 
-# Create your models here.
+
+class Instituicao(Model):
+    nome = CharField(max_length=100, primary_key=True)
+
+
+class Acao(Model):
+    codigo = CharField(max_length=10, primary_key=True)
+    instituicao = OneToOneField(Instituicao, on_delete=CASCADE)
+    data_atualizacao = DateField()
+    quantidade = IntegerField()
+    preco = FloatField()
+    
+    @property
+    def valor_total(self):
+        return self.preco * self.quantidade
