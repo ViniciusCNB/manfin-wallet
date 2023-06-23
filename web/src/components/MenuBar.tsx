5bfde860ca2dbe40c5400ad8e62c5379802799d3
Vinícius <viniborg25@gmail.com>
2023-06-23 13:02:31 -0300

    mudanças iniciais no front
    removidos os arquivos desnecessários e inicio da estilização da página inicial

---
+++ web/src/components/MenuBar.tsx
@@ -0,0 +1,15 @@
+import "../styles/index.css"
+
+const MenuBar = () => {
+  return (
+    <>
+      <div className="bg-blue-900 m-5 py-5 px-10 flex rounded-xl gap-10">
+        <span className="siteTitle typingAnimation">investment.portfolio</span>
+        <div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default MenuBar
