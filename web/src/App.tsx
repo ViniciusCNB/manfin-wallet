bb90847d0150bd50eca5877282a28d59b663114f
Vinícius <viniborg25@gmail.com>
2023-06-30 11:39:28 -0300

    Criando uma página para o erro 404

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,13 @@
 import HomePage from "./pages/HomePage"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import "./styles/main.css"
+import PageNotFoundPage from "./pages/PageNotFoundPage"
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+    errorElement: <PageNotFoundPage />
   },
 ])
 
---
+++ web/src/pages/PageNotFoundPage.tsx
@@ -0,0 +1,19 @@
+const PageNotFoundPage = () => {
+  return (
+    <>
+      <div className="bg-gradient-to-l from-[#033959] to-[#01141f] h-screen w-screen text-white p-24 text-6xl grid grid-cols-2 gap-14 border-b-2 border-r-2">
+        <div className="flex flex-col gap-5 justify-center items-center">
+          <div>ERRO 404</div>
+          <div>Página não encontrada.</div>
+        </div>
+        <div className="flex justify-center items-center">
+          <span className="text-white text-[5.5rem] border-b-2 h-fit py-2">
+            ManFin Wallet
+          </span>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default PageNotFoundPage
