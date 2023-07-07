31f61ec06379bc733e580991073db92ae2b6a86e
Vinícius <viniborg25@gmail.com>
2023-07-07 17:16:35 -0300

    Formatação da tabela que exibe as aplicações cadastradas no portfólio

--- web/src/components/Portfolio/StockCard.tsx
+++ web/src/components/Portfolio/StockCard.tsx
@@ -17,51 +17,59 @@ const StockCard = (props: StockCardProps) => {
 
   return (
     <>
-      <div className="w-full grid grid-cols-7 py-2 text-center divide-x-2 divide-[#01141f]">
-        <div className="flex justify-center">
-          <p>
-            <span className="font-bold text-[#01141f]">Ativo</span> <br />
-            {props.codigo}
-          </p>
+      <div className="flex flex-row divide-x-2 divide-[#01141f]">
+        <div className="w-full grid grid-cols-6 py-2 text-center divide-x-2 divide-[#01141f]">
+          <div className="flex justify-center">
+            <p>
+              <span className="font-bold text-[#01141f]">Ativo</span> <br />
+              {props.codigo}
+            </p>
+          </div>
+          <div className="flex justify-center">
+            <p>
+              <span className="font-bold text-[#01141f]">Data da Compra</span>
+              <br /> {props.data_atualizacao}
+            </p>
+          </div>
+          <div className="flex justify-center">
+            <p>
+              <span className="font-bold text-[#01141f]">Instituição</span>{" "}
+              <br />
+              {props.instituicao}
+            </p>
+          </div>
+          <div className="flex justify-center">
+            <p>
+              <span className="font-bold text-[#01141f]">Preço</span> <br /> R${" "}
+              {props.preco.toLocaleString("pt-BR", {
+                minimumFractionDigits: 2,
+              })}
+            </p>
+          </div>
+          <div className="flex justify-center">
+            <p>
+              <span className="font-bold text-[#01141f]">Quantidade</span>{" "}
+              <br />
+              {props.quantidade}
+            </p>
+          </div>
+          <div className="flex justify-center">
+            <p>
+              <span className="font-bold text-[#01141f]">Saldo Atual</span>{" "}
+              <br />
+              R${" "}
+              {props.valor_total.toLocaleString("pt-BR", {
+                minimumFractionDigits: 2,
+              })}
+            </p>
+          </div>
         </div>
-        <div className="flex justify-center">
-          <p>
-            <span className="font-bold text-[#01141f]">Data da Compra</span>
-            <br /> {props.data_atualizacao}
-          </p>
-        </div>
-        <div className="flex justify-center">
-          <p>
-            <span className="font-bold text-[#01141f]">Instituição</span> <br />
-            {props.instituicao}
-          </p>
-        </div>
-        <div className="flex justify-center">
-          <p>
-            <span className="font-bold text-[#01141f]">Preço</span> <br /> R${" "}
-            {props.preco.toLocaleString("pt-BR", {
-              minimumFractionDigits: 2,
-            })}
-          </p>
-        </div>
-        <div className="flex justify-center">
-          <p>
-            <span className="font-bold text-[#01141f]">Quantidade</span> <br />
-            {props.quantidade}
-          </p>
-        </div>
-        <div className="flex justify-center">
-          <p>
-            <span className="font-bold text-[#01141f]">Saldo Atual</span> <br />
-            R${" "}
-            {props.valor_total.toLocaleString("pt-BR", {
-              minimumFractionDigits: 2,
-            })}
-          </p>
-        </div>
-        <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center my-2 px-4">
           <Dialog.Root open={open} onOpenChange={setOpen}>
-            <Dialog.Trigger className="hover:bg-gray-400 p-3 rounded-[50%]">
+            <Dialog.Trigger
+              className="hover:bg-gray-400 p-3 rounded-[50%]"
+              title="Apagar Aplicação"
+            >
               <Trash size={20} weight="bold" />
             </Dialog.Trigger>
 
