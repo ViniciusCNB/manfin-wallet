7e6fd0af7eeaf7ad962c01eeb4b7ef42963f7dcf
Vinícius Borges <viniborg25@gmail.com>
2023-09-04 18:45:45 -0300

    Atualização do modal de adição de nova despesa

--- web/src/components/Expenses/AddExpenseModal.tsx
+++ web/src/components/Expenses/AddExpenseModal.tsx
@@ -7,6 +7,7 @@ import { useEffect, useState } from "react"
 const AddExpenseModal = () => {
   const { register, handleSubmit, reset } = useForm()
   const [categorias, setCategorias] = useState([])
+  const [formasPagamento, setFormasPagamento] = useState([])
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const onSubmit = async (data: any) => {
@@ -35,12 +36,19 @@ const AddExpenseModal = () => {
       .then((data) => setCategorias(data))
   }, [])
 
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:8000/forma-pagamento/")
+      .then((response) => response.data)
+      .then((data) => setFormasPagamento(data))
+  }, [])
+
   return (
     <>
       <Dialog.Portal>
         <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
 
-        <Dialog.Content className="fixed bg-[#bbbbbb] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[30rem] h-[42rem] shadow-lg shadow-black/25">
+        <Dialog.Content className="fixed bg-[#bbbbbb] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[30rem] h-[48rem] shadow-lg shadow-black/25">
           <Dialog.Title className="bg-[#187c44] rounded-lg uppercase py-4 text-2xl text-center font-extrabold mb-10 shadow-lg shadow-black/25">
             Adicionar Nova Despesa
           </Dialog.Title>
@@ -118,12 +126,33 @@ const AddExpenseModal = () => {
                   htmlFor=""
                   className="font-bold text-[16px] text-[#187c44] uppercase"
                 >
-                  Pagamento
+                  Forma de Pagamento
+                </label>
+                <select
+                  {...register("forma_pagamento", { required: true })}
+                  className="bg-gray-200 text-black rounded py-3 px-4 shadow-xl"
+                >
+                  <option value=""></option>
+                  {formasPagamento.map((forma_pagamento) => (
+                    <option key={forma_pagamento["id"]} value={forma_pagamento["id"]}>
+                      {forma_pagamento["descricao"]}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+            <div className="flex flex-col gap-2">
+              <div className="flex flex-col mb-5">
+                <label
+                  htmlFor=""
+                  className="font-bold text-[16px] text-[#187c44] uppercase"
+                >
+                  Observação (opcional)
                 </label>
                 <input
                   type="text"
                   className="bg-gray-200 text-black rounded py-3 px-4 shadow-xl"
-                  {...register("pagamento", { required: true })}
+                  {...register("observacao", { required: false })}
                 />
               </div>
             </div>
