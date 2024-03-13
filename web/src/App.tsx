11351b1e11f796346859f848cde41978da0bbf07
Vinícius Borges <viniborg25@gmail.com>
2024-03-12 21:28:06 -0300

    Create Footer, Menu and NavBar components

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,29 +1,55 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom"
 import "./styles/main.css"
 import HomePage from "./pages/HomePage"
 import NotFoundPage from "./pages/NotFoundPage"
 import PortfolioPage from "./pages/PortfolioPage"
 import ApplicationPage from "./pages/ApplicationPage"
 import ExpensesPage from "./pages/ExpensesPage"
+import NavBar from "./components/NavBar"
+import Menu from "./components/Menu"
+import Footer from "./components/Footer"
+
+const Layout = () => {
+  return (
+    <div className="main">
+      <NavBar />
+      <div className="container">
+        <div className="menuContainer">
+          <Menu />
+        </div>
+        <div className="contentContainer">
+          <Outlet />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  )
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
-    errorElement: <NotFoundPage />
+    errorElement: <NotFoundPage />,
   },
   {
-    path: "/portfolio",
-    element: <PortfolioPage />
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/portfolio",
+        element: <PortfolioPage />,
+      },
+      {
+        path: "/portfolio/:codigo",
+        element: <ApplicationPage />,
+      },
+      {
+        path: "/despesas",
+        element: <ExpensesPage />,
+      },
+    ],
   },
-  {
-    path: "/portfolio/:codigo",
-    element: <ApplicationPage />
-  },
-  {
-    path: "/despesas",
-    element: <ExpensesPage />
-  }
 ])
 
 const App = () => {
